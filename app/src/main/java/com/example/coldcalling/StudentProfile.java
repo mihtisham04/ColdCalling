fd2150836fa72162843df26a4a28f7f416b502fd
Sebas23456 <smarinescuny@gmail.com>
2021-10-18 18:32:52 -0400

    Committing?

---
+++ app/src/main/java/com/example/coldcalling/StudentProfile.java
@@ -0,0 +1,19 @@
+package com.example.coldcalling;
+
+public class StudentProfile {
+
+    private String mStudentName;
+    private int mTimeWhenCalled;
+
+
+    private StudentProfile(String StudentName, int TimeWhenCalled) {
+        mStudentName = StudentName;
+        mTimeWhenCalled = TimeWhenCalled;
+    }
+
+
+
+
+
+
+}
