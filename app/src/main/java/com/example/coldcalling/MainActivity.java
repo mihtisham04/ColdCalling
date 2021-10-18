9d712fb59ffe986c0143ad9dc5177db423de93eb
Sebas23456 <smarinescuny@gmail.com>
2021-10-18 18:32:39 -0400

    Committing?

--- app/src/main/java/com/example/coldcalling/MainActivity.java
+++ app/src/main/java/com/example/coldcalling/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.coldcalling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView CurrentTimeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CurrentTimeView =(TextView)findViewById(R.id.CurrentTimeView);
+        String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
+        CurrentTimeView.setText(currentDateTimeString);
+
+
+    }
+
+    public chooseStudent() {
+        int num = (int)(Math.random()*33+1);
+        System.out.println("Method Executed:chooseStudent");
+
     }
 
 }
