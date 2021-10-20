bd3221e09caab04e17636c363b7406cb1d6464b0
Sebas23456 <smarinescuny@gmail.com>
2021-10-20 09:24:05 -0400

    Committing?

--- app/src/main/java/com/example/coldcalling/MainActivity.java
+++ app/src/main/java/com/example/coldcalling/MainActivity.java
@@ -55,20 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void NameGather(int numStudents) {
-         studentList = new StudentProfile[numStudents];
 
-
-
-
-        StudentProfile temp = StudentProfile[num];
-
-        Image Temp = temp.returnProfilePic();
-        ProfilePicView.setImageResource(temp.returnProfilePic());
-
-        StudentNameView.setText(temp.returnStudentName());
-
-
-    }
 
 }
