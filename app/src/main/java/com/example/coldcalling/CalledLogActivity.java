81bc116b3841bea7af37a6f30d8dd725cc58d9d1
Sebas23456 <smarinescuny@gmail.com>
2021-10-23 18:48:50 -0400

    I have added the java files for the two other activities that I also added

---
+++ app/src/main/java/com/example/coldcalling/CalledLogActivity.java
@@ -0,0 +1,16 @@
+package com.example.coldcalling;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+
+public class CalledLogActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.called_activity);
+
+    }
+}
