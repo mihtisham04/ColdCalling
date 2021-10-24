640485ef09b1c867eac57a1e2885cb0b494dfd18
Sebas23456 <smarinescuny@gmail.com>
2021-10-23 23:05:02 -0400

    i did some stuff

--- app/src/main/java/com/example/coldcalling/CalledLogActivity.java
+++ app/src/main/java/com/example/coldcalling/CalledLogActivity.java
@@ -1,12 +1,19 @@
 package com.example.coldcalling;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 
 public class CalledLogActivity extends AppCompatActivity {
+
+    private TextView CalledTextView;
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/coldcalling/UncalledLogActivity.java
+++ app/src/main/java/com/example/coldcalling/UncalledLogActivity.java
@@ -1,14 +1,39 @@
 package com.example.coldcalling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UncalledLogActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+import java.util.ArrayList;
 
+public class UncalledLogActivity extends AppCompatActivity {
+
+    private TextView UnCalledTextView;
+    private static ArrayList<StudentProfile> placeholder;
+    private Button backButton;
+
+    public static Intent newIntent(MainActivity packageContext, ArrayList<StudentProfile> input) {
+        Intent i = new Intent(packageContext, UncalledLogActivity.class);
+        return i;
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uncalled_activity);
+
+        UnCalledTextView = (TextView) findViewById(R.id.UnCalledTextBox);
+
+
+
+
+
+
+
+
+
 
     }
 
